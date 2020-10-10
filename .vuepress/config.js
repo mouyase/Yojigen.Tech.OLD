b2082a663f43c438f88a11d5c027c809dc17e85d
某亚瑟 <i@yojigen.tech>
2020-10-11 07:34:44 +0800

    Update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,101 +1,101 @@
 module.exports = {
-  "title": "四次元科技",
-  "description": "aa",
-  "dest": "public",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
-      "meta",
-      {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
-  ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
-      {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
-        ]
-      },
-      {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://github.com/recoluan",
-            "icon": "reco-github"
-          }
-        ]
-      }
-    ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
+    port: 62000,
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
     },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
-      },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
-    },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "四次元科技",
-        "desc": "有关二次元、三次元、科技、生活以及一切",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
+    title: "四次元科技",
+    description: "有关二次元、三次元、科技、生活以及一切",
+    dest: "public",
+    head: [
+        [
+            "link",
+            {
+                "rel": "icon",
+                "href": "/favicon.ico"
+            }
+        ],
+        [
+            "meta",
+            {
+                name: "viewport",
+                content: "width=device-width,initial-scale=1,user-scalable=no"
+            }
+        ]
     ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "mouyase",
-    "authorAvatar": "/avatar.png",
-    "record": "xxxx",
-    "startYear": "2017"
-  },
-  "markdown": {
-    "lineNumbers": true
-  }
+    theme: "reco",
+    themeConfig: {
+        nav: [
+            {
+                text: "首页",
+                link: "/",
+                icon: "reco-home"
+            },
+            {
+                text: "时间线",
+                link: "/timeline/",
+                icon: "reco-date"
+            },
+            // {
+            //     text: "文档",
+            //     icon: "reco-message",
+            //     "items": [
+            //         {
+            //             text: "vuepress-reco",
+            //             link: "/docs/theme-reco/"
+            //         }
+            //     ]
+            // },
+            {
+                text: "联系方式",
+                icon: "reco-message",
+                items: [
+                    {
+                        text: "GitHub",
+                        link: "https://github.com/mouyase",
+                        icon: "reco-github"
+                    }
+                ]
+            }
+        ],
+        sidebar: {
+            "/docs/theme-reco/": [
+                "",
+                "theme",
+                "plugin",
+                "api"
+            ]
+        },
+        type: "blog",
+        blogConfig: {
+            category: {
+                location: 2,
+                text: "分类"
+            },
+            tag: {
+                location: 3,
+                text: "标签"
+            }
+        },
+        friendLink: [
+            {
+                title: "午后南杂",
+                desc: "Enjoy when you can, and endure when you must.",
+                email: "1156743527@qq.com",
+                link: "https://www.recoluan.com"
+            },
+        ],
+        logo: "/img/logo.jpg",
+        search: true,
+        searchMaxSuggestions: 10,
+        lastUpdated: "最后更新",
+        author: "某亚瑟",
+        authorAvatar: "/img/avatar.jpg",
+        record: "四次元科技",
+        startYear: "2015"
+    },
+    markdown: {
+        "lineNumbers": true
+    }
 }
---
+++ index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroText: 四次元科技
+tagline: 有关二次元、三次元、科技、生活以及一切
+bgImageStyle: {
+  height: '200px'
+}
+---
