4765af93860325adfa5cb7dc60efc016b19895fa
某亚瑟 <i@yojigen.tech>
2020-10-10 23:59:38 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,101 @@
+module.exports = {
+  "title": "四次元科技",
+  "description": "aa",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Docs",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "四次元科技",
+        "desc": "有关二次元、三次元、科技、生活以及一切",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "mouyase",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
