51ce46e64db3545e6283d1711e6504b4cb58ea0e
某亚瑟 <i@yojigen.tech>
2020-10-16 02:20:25 +0800

    Update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,30 @@
+const path = require('path')
+
 module.exports = {
     port: 62000,
+    configureWebpack: () => {
+        const NODE_ENV = process.env.NODE_ENV
+        if (NODE_ENV === 'production') {
+            return {
+                output: {
+                    publicPath: 'https://cdn.jsdelivr.net/gh/mouyase/Yojigen.Tech@gh-pages/'
+                },
+                resolve: {
+                    alias: {
+                        'public': path.resolve(__dirname, './public')
+                    }
+                }
+            }
+        } else {
+            return {
+                resolve: {
+                    alias: {
+                        'public': path.resolve(__dirname, './public')
+                    }
+                }
+            }
+        }
+    },
     locales: {
         '/': {
             lang: 'zh-CN'
